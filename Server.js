04c9fa3ac3fac0121da7733d5cf1a533172ad0c4
gara0xqm <gara.xq@gmail.com>
2025-12-01 01:26:00 +0200

    production

--- Server.js
+++ Server.js
@@ -1,5 +1,7 @@
 const express = require('express')
-require("dotenv").config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const connectDB = require('./API/config/db')
 const userRoutes = require('./API/features/user/routes/user_route');
 const app = express()
